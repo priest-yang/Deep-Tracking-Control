06f8e95e07390d10d610ac4b8c1d6a3fd901c8e3
Shaoze Yang <priest-yang@sjtu.edu.cn>
2024-06-28 22:28:42 +0800

    update x30 model

--- legged_gym/envs/x30/x30_dtc_config.py
+++ legged_gym/envs/x30/x30_dtc_config.py
@@ -1,13 +1,13 @@
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
-class Lite3DTCCfg( LeggedRobotCfg ):
-    #! added by wz
+class X30DTCCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env ):
         # num_envs = 4096
-        num_envs = 8
-        num_observations = 45 + 8 # 45 + PMTG:13=58
-        num_privileged_obs =  693 + 3 + 693  #212+384
-        num_obs_history = (45 + 8)*5
+        #! changed by wz
+        num_envs = 4096 * 3
+        num_observations = 45 # 45 + PMTG:13=58
+        num_privileged_obs =  693 +3 +693 #212+384
+        num_obs_history = 45*5
         num_observation_history = 5 # the length of history ,for lstm  =1
         debug_viz = False
 
@@ -16,7 +16,7 @@ class Lite3DTCCfg( LeggedRobotCfg ):
         play_teacher = True
         num_lidar = 3200
         num_lidar_history = 5
-        
+
     class terrain:
         mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
         # mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
@@ -58,101 +58,66 @@ class Lite3DTCCfg( LeggedRobotCfg ):
         # trimesh only:
         slope_treshold = 0.75 # slopes above this threshold will be corrected to vertical surfaces
 
-
-
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.4] # x,y,z [m]
+        pos = [0.0, 0.0, 0.51] # x,y,z [m]
+        pos_z_range = [0.5, 0.51]
         default_joint_angles = { # = target angles [rad] when action = 0.0
-            'FL_HipX_joint': 0.1,   # [rad]
-            'HL_HipX_joint': 0.1,   # [rad]
-            'FR_HipX_joint': -0.1 ,  # [rad]
-            'HR_HipX_joint': -0.1,   # [rad]
+            'FL_HipX_joint': 0.1,   # [rad]0.1
+            'HL_HipX_joint': 0.1,   # [rad]0.1
+            'FR_HipX_joint': -0.1 ,  # [rad]-0.1
+            'HR_HipX_joint': -0.1,   # [rad]-0.1
 
-            # 'FL_HipX_joint': 0.0,   # [rad]
-            # 'HL_HipX_joint': 0.0,   # [rad]
-            # 'FR_HipX_joint': -0.0 ,  # [rad]
-            # 'HR_HipX_joint': -0.0,   # [rad]
+            'FL_HipY_joint': -0.715,     # [rad]
+            'HL_HipY_joint': -0.715,   # [rad]
+            'FR_HipY_joint': -0.715,     # [rad]
+            'HR_HipY_joint': -0.715,   # [rad]
 
-            'FL_HipY_joint': -1.,     # [rad]
-            'HL_HipY_joint': -1.,   # [rad]
-            'FR_HipY_joint': -1.,     # [rad]
-            'HR_HipY_joint': -1.,   # [rad]
-
-            'FL_Knee_joint': 1.8,   # [rad]
-            'HL_Knee_joint': 1.8,    # [rad]
-            'FR_Knee_joint': 1.8,  # [rad]
-            'HR_Knee_joint': 1.8,    # [rad]
+            'FL_Knee_joint': 1.43,   # [rad]
+            'HL_Knee_joint': 1.43,    # [rad]
+            'FR_Knee_joint': 1.43,  # [rad]
+            'HR_Knee_joint': 1.43,    # [rad]
         }
 
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
         control_type = 'P'
-        stiffness = {'joint': 25.}  # [N*m/rad]
-        damping = {'joint': 0.5}     # [N*m*s/rad]
+        stiffness = {'HipX': 120. , 'HipY': 120. ,'Knee': 150. }  # [N*m/rad]
+        damping = {'HipX': 3. , 'HipY': 3. ,'Knee': 3.5 }     # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.25
         # decimation: Number of control action updates @ sim DT per policy DT
         decimation = 4
 
     class asset( LeggedRobotCfg.asset ):
-        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/Lite3/urdf/Lite3.urdf'
-        name = "Lite3"
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/X30/urdf/X30.urdf'
+        name = "X30"
         foot_name = "FOOT"
-        
         penalize_contacts_on = ["THIGH", "SHANK"]
         collision_state = ["TORSO","THIGH", "SHANK"]
         terminate_after_contacts_on = ["TORSO"]
-        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
-    
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        flip_visual_attachments = False
+
+  
     class rewards( LeggedRobotCfg.rewards ):
-        soft_dof_pos_limit = 0.9
-        # base_height_target = 0.32
-        base_height_target = 0.34
+        soft_dof_pos_limit = 0.95
+        base_height_target = 0.49
         class scales( LeggedRobotCfg.rewards.scales ):
-            torques = -0.000001
+            torques = -0.000001#-0.0002
             dof_pos_limits = -10.0
 
-            #######################################
-            #! train first
-            # feet_air_time = 0.0
-            #! train after
-            # feet_air_time = 1.0
-            #######################################
-
-            #######################################
-            # #!step 2: unlock abilitity(seems impossible 0.23cm std:0.62 level:6.07) (wz_unbelievable)
-            torques = -0.000001
-            dof_pos_limits = -10.0
-            dof_acc = -2.5e-7 / 10
-            ang_vel_xy = - 0.05 / 10
-            lin_vel_z = - 2.0 / 10
-            smooth = -0.015 / 10
-            feet_air_time = 1.0
-             #######################################
-            
-
-            #######################################
-            #!step 3: try from 0.23cm std:0.62 level:6.10 (wz_unbelievable_2 !!!has been tested)
-            # torques = -0.000001
-            # dof_pos_limits = -10.0
-            # ang_vel_xy = - 0.05 / 10
-            # lin_vel_z = - 2.0 / 10
-            # smooth = -0.015 / 10
-            # feet_air_time = 1.0
-            #######################################
-            tracking_optimal_footholds = 0.08
-
-
-class Lite3DTCCfgPPO( LeggedRobotCfgPPO ):
-
+class X30RoughCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
-        entropy_coef = 0.003
+        entropy_coef = 0.01
     class runner( LeggedRobotCfgPPO.runner ):
         run_name = ''
-        experiment_name = 'lite3_dtc'
-        max_iterations = 20000
+        experiment_name = 'rough_X30'
+        max_iterations = 30000
 
-        # #! added by wz
-        # resume =  True
-        # checkpoint = -1 # -1 = last saved model       
+        resume =  True
+        load_run = '/home/wt/deeprobotics/dr_gym_vae/logs/rough_X30/May22_11-36-13_'
+        checkpoint = -1#2600#8000#'7600' # -1 = last saved model        
 
+   
+
+  
